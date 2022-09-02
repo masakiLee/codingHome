0b672a2d2930a69ec667a9a1d7aaf36092f312b3
masakilee <everything2427@hotmail.com>
2022-09-02 14:14:03 +0800

    Update 2022-09-02T06:13:59.568Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,18 +1,19 @@
-const swiper = new Swiper(".swiper", {
+"use strict";
+
+var swiper = new Swiper(".swiper", {
   // Optional parameters
   effect: "fade",
   fadeEffect: {
-    crossFade: true,
+    crossFade: true
   },
-
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination"
   },
-
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    prevEl: ".swiper-button-prev"
+  }
 });
+//# sourceMappingURL=all.js.map
